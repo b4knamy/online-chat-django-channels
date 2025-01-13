92248ed067fcfcaaa7ae82235948470afb9caa20
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-13 02:06:49 -0300

    some updates

--- back-end/chat/management/commands/create_users.py
+++ back-end/chat/management/commands/create_users.py
@@ -10,28 +10,30 @@ class Command(BaseCommand):
         parser.add_argument("--og", action="store_true")
 
     def handle(self, *args, **kwargs):
-        usernames = ["Basch", "Vaan", "Fran", "Ashe", "Penelo", "Balthier", "Baknamy"]
-        password = make_password("fkUser10@!")
-        for username in usernames:
-            if username == "Baknamy":
-                new_user = User.objects.create(
-                username=username,
-                password=password,
-                is_superuser=True
-            )
-            else:
-                new_user = User.objects.create(
-                    username=username,
-                    password=password
-                )
-            new_user.save()
 
-        user = User.objects.get(username="Baknamy")
-        main_room = Room.objects.create(
-            admin=user,
-            name="Lobby"
-        )
-        main_room.save()
-        self.stdout.write(self.style.SUCCESS("Dados criados!"))
+        if not User.objects.filter(username="Baknamy").first():
+            usernames = ["Basch", "Vaan", "Fran", "Ashe", "Penelo", "Balthier", "Baknamy"]
+            password = make_password("fkUser10@!")
+            for username in usernames:
+                if username == "Baknamy":
+                    new_user = User.objects.create(
+                    username=username,
+                    password=password,
+                    is_superuser=True
+                )
+                else:
+                    new_user = User.objects.create(
+                        username=username,
+                        password=password
+                    )
+                new_user.save()
+
+            user = User.objects.get(username="Baknamy")
+            main_room = Room.objects.create(
+                admin=user,
+                name="Lobby"
+            )
+            main_room.save()
+            self.stdout.write(self.style.SUCCESS("Dados criados!"))
 
 
